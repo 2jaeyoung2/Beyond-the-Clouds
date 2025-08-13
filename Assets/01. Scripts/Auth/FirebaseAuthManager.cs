d26d019b0f09af625fad8f987a3afba3e353714a
2jaeyoung2 <80027417+2jaeyoung2@users.noreply.github.com>
2025-08-13 15:24:24 +0900

    [Temp]Auth in progress
    probably will use Steamworks

--- Assets/01. Scripts/Auth/FirebaseAuthManager.cs
+++ Assets/01. Scripts/Auth/FirebaseAuthManager.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using Firebase.Database;
 
 public class FirebaseAuthManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static FirebaseAuthManager Instance { get; private set; }
 
-    // Update is called once per frame
-    void Update()
+    public static FirebaseUser User { get; private set; }
+
+    public static DatabaseReference DbRef { get; private set; }
+
+    private FirebaseAuth auth;
+
+    private void Awake()
     {
-        
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+
+            return;
+        }
+        else
+        {
+            Instance = this;
+
+            DontDestroyOnLoad(gameObject);
+        }
+
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            DependencyStatus dependencyStatus = task.Result;
+
+            if (dependencyStatus == DependencyStatus.Available)
+            {
+                auth = FirebaseAuth.DefaultInstance;
+
+                DbRef = FirebaseDatabase.DefaultInstance.RootReference;
+            }
+            else
+            {
+                Debug.Log("Firebase ¿À·ù");
+            }
+        });
     }
 }
