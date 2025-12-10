d7a97aa140d0c9c8a5cec0f76be84bd43e07e327
2jaeyoung2 <i0579923@gmail.com>
2025-12-10 21:09:49 +0900

    [Temp]preparing to start

--- Assets/01. Scripts/Player/Player Manager & Stats/JumpStats.cs
+++ Assets/01. Scripts/Player/Player Manager & Stats/JumpStats.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FirebaseAuthUIController : MonoBehaviour
+public class JumpStats : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- Assets/01. Scripts/Player/PlayerMovement.cs
+++ Assets/01. Scripts/Player/PlayerMovement.cs
@@ -50,7 +50,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void Start()
     {
-        MoveSpeed = player.MoveSpeed;
+        MoveSpeed = 5f;
     }
 
     private void FixedUpdate()
